b950c46bd385045e76182192a9c17e5078cb837c
Lacy Jane <lacyburnette50@gmail.com>
2017-04-17 18:07:59 -0500

    Done!!! Yay!!!

---
+++ src/main/java/org/launchcode/models/Category.java
@@ -0,0 +1,43 @@
+package org.launchcode.models;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by Lacy Jane on 4/12/2017.
+ */
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=3, max=15)
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
+
+    public Category() { }
+
+    public Category(String aName) {
+        this.name = aName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
